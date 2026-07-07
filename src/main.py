2596313d3f4e75b67e6f629afcab88ee82d758f7
yashchaugale <chaugaleyash2003@gmail.com>
2026-07-08 02:20:17 +0530

    Integrate groq api

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-print("Welcome to Agentic Code Assistant 🚀")
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+# Load environment variables
+load_dotenv()
+
+# Create Groq client
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+# Send a message
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role": "user",
+            "content": "Introduce yourself in one sentence."
+        }
+    ]
+)
+
+print(response.choices[0].message.content)

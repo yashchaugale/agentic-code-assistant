575c6c77e7427b9439a86b194b7bb964874d316f
yashchaugale <chaugaleyash2003@gmail.com>
2026-07-10 00:54:59 +0530

    added tool logic to assistent agent

--- src/agents/assistant.py
+++ src/agents/assistant.py
@@ -4,12 +4,28 @@ from chat import (
     get_conversation
 )
 
-from llm import ask_llm
+from llm import ask_llm, summarize_file
+from tools.file_reader import read_file
 
 
 class AssistantAgent:
 
     def chat(self, user_input):
+
+        # ---------------- FILE TOOL ----------------
+
+        if user_input.startswith("read "):
+
+            filename = user_input.replace("read ", "").strip()
+
+            file_content = read_file(filename)
+
+            if file_content is None:
+                return "❌ File not found."
+
+            return summarize_file(file_content)
+
+        # ---------------- NORMAL CHAT ----------------
 
         add_user_message(user_input)
 
--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
-from llm import summarize_file
 from agents.assistant import AssistantAgent
-from tools.file_reader import read_file
 
 agent = AssistantAgent()
 
 print("=" * 50)
 print("🤖 Agentic Code Assistant")
 print("Type 'exit' to quit.")
-print("Type 'read README.md' to summarize a file.")
 print("=" * 50)
 
 while True:
@@ -18,29 +15,6 @@ while True:
         print("\n👋 Goodbye!")
         break
 
-    # ---------- FILE TOOL ----------
-    if user_input.startswith("read "):
-
-        filename = user_input.replace("read ", "").strip()
-
-        file_content = read_file(filename)
-
-        if file_content is None:
-            print("\n❌ File not found.")
-            continue
-
-        answer = summarize_file(file_content)
-
-        print("\nAI:", answer)
-
-        continue
-
-    # ---------- NORMAL CHAT ----------
-
-    
-
     answer = agent.chat(user_input)
 
     print("\nAI:", answer)
-
-    
